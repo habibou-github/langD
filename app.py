85f65664ea91b7914a249aa09f9d13315c8b7f8d
infoh <info.habibou@gmail.com>
2022-06-02 00:55:02 +0100

    convert simple app to langDetect app

--- app.py
+++ app.py
@@ -1,11 +1,35 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, request
+from langdetect import detect
+
+all_language_codes = {
+    "en": "English",
+    "fr": "French",
+    "ar": "Arabic",
+    "de": "Germane",
+    "ru": "Russian"
+}
 
 app = Flask(__name__)
 
+
 @app.route('/')
+@app.route('/home')
 def home():
-    return "Hamza habibou"
+    return render_template("index.html")
 
 
-if __name__ == '__main__':
-    app.run()
+@app.route('/result', methods=['POST', 'GET'])
+def result():
+    output = request.form.to_dict()
+    print(output)
+    out = output["name"]
+    name = ''
+    if out:
+        lang = detect(out)
+        name = all_language_codes.get(lang)
+
+    return render_template('index.html', name=name, out=out)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
